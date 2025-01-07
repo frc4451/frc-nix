5954e1ef8c49236865fa33bb4e39f9243b9be9ba
Lewis Arias <lewisgarias@outlook.com>
2025-01-06 21:04:01 -0500

    elastic-dashboard: 2024.2.0 -> 2025.0.1 (can't figure out how to make it build from source sadly)

--- pkgs/elastic-dashboard/default.nix
+++ pkgs/elastic-dashboard/default.nix
@@ -1,44 +1,45 @@
-{ lib
-, flutter319
-, fetchFromGitHub
-, copyDesktopItems
-, makeDesktopItem
+{ stdenv
+, fetchurl
+, autoPatchelfHook
+, wrapGAppsHook3
+, lib
+, unzip
 ,
 }:
-flutter319.buildFlutterApplication rec {
+stdenv.mkDerivation rec {
   pname = "elastic-dashboard";
-  version = "2024.2.0";
+  version = "2025.0.1";
 
-  src = fetchFromGitHub {
-    owner = "Gold872";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-u8pqkaChSD01DBdbn43txIQBvfRQ8lLmeT7rbGdVJcE=";
+  src = fetchurl {
+    url = "https://github.com/Gold872/elastic-dashboard/releases/download/v${version}/Elastic-Linux.zip";
+    hash = "sha256-891++x9dS3/DAbCWq1mT3AE6Rsnqy6ZqFGMAgcDP7uI=";
   };
 
-  pubspecLock = lib.importJSON ./pubspec.lock.json;
-
-  nativeBuildInputs = [ copyDesktopItems ];
-
-  # Get everything out of $out/app to avoid conflicts with other flutter packages
-  postInstall = ''
-    mkdir "$out"/opt
-    mv "$out"/app "$out"/opt/${pname}
-    ln -sf "$out"/opt/${pname}/elastic_dashboard "$out"/bin/elastic_dashboard
-    install -Dm444 ${src}/assets/logos/logo.png "$out"/share/pixmaps/${pname}.png
-  '';
-
-  desktopItems = [
-    (makeDesktopItem {
-      desktopName = "Elastic";
-      name = pname;
-      exec = "elastic_dashboard";
-      icon = pname;
-      comment = meta.description;
-      categories = [ "Development" ];
-      keywords = [ "FRC" "Dashboard" ];
-    })
+  nativeBuildInputs = [
+    autoPatchelfHook
+    wrapGAppsHook3
+    unzip
   ];
+
+  sourceRoot = ".";
+  unpackCmd = ''unzip "$src"'';
+
+  installPhase = ''
+    runHook preInstall
+
+    # make the needed directories
+    mkdir -p "$out"/bin
+    mkdir -p "$out"/opt/${pname}
+
+    # copy over program files
+    cp -r ./* "$out"/opt/${pname}
+
+    chmod +x "$out"/opt/${pname}/elastic_dashboard
+
+    ln -s "$out"/opt/${pname}/elastic_dashboard "$out"/bin/elastic_dashboard
+
+    runHook postInstall
+  '';
 
   meta = with lib; {
     mainProgram = "elastic_dashboard";
