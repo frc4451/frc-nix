91607fe81a7d0acd70ff3083f9e64c8f0847fd7a
me-it-is <188520719+me-it-is@users.noreply.github.com>
2025-08-24 10:51:30 -0700

    ascope biuild from source

---
+++ pkgs/advantagescope/docs.nix
@@ -0,0 +1,22 @@
+{
+  buildNpmPackage,
+  src,
+  pname,
+  version,
+  ...
+}:
+buildNpmPackage (finalAttrs: {
+  pname = pname + "-docs";
+  inherit version src;
+
+  sourceRoot = "${finalAttrs.src.name}/docs";
+  npmDepsHash = "sha256-HQW8MXBRnVATMKwmhZQbaOb1Img/q0BUXOR4A3BlMDE=";
+
+  buildPhase = ''
+    npm run build-embed
+  '';
+
+  installPhase = ''
+    cp -r ./build $out
+  '';
+})
