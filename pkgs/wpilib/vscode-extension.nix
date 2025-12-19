42d97e4722d86e9e1908378c1e82c52ee87cbe23
Lewis Arias <lewisgarias@outlook.com>
2025-12-19 04:56:42 -0500

    wpilib-vscode: more properly explain last commit that fixed the unpacking

--- pkgs/wpilib/vscode-extension.nix
+++ pkgs/wpilib/vscode-extension.nix
@@ -13,6 +13,11 @@ vscode-utils.buildVscodeExtension rec {
   src = fetchurl {
     url = "https://github.com/wpilibsuite/vscode-wpilib/releases/download/v${version}/vscode-wpilib-${version}.vsix";
     hash = "sha256-HAnN+1IYbma3v+sPyTivqDCIOdyClLz7FP/chlZ0SCI=";
+    # TODO: Once the version of nixpkgs in this flake is updated we should remove
+    # the custom `name` and the `unzip` nativeBuildInput
+    # See: https://github.com/NixOS/nixpkgs/commit/e24a734076ea21365bb618d63f5c9a70006dd196
+
+    # For compatibility older versions of nixpkgs
     # The `*.vsix` file is in the end a simple zip file. Change the extension
     # so that existing `unzip` hooks takes care of the unpacking.
     name = "${name}.zip";
