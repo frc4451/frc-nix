e9f8e2264cb5cf2567f03a4377e2845854f9f1e5
NullCube <nullcub3@gmail.com>
2026-01-21 10:47:00 -0800

    feat: use packagesFromDirectoryRecursive

--- default.nix
+++ default.nix
@@ -1,16 +1,37 @@
 {
   pkgs ? import <nixpkgs>,
-  prev ? pkgs,
 }:
-rec {
-  advantagescope = pkgs.callPackage ./pkgs/advantagescope { };
-  choreo = pkgs.callPackage ./pkgs/choreo { };
-  elastic-dashboard = pkgs.callPackage ./pkgs/elastic-dashboard { };
-  pathplanner = pkgs.callPackage ./pkgs/pathplanner { };
-  wpilib = pkgs.lib.recurseIntoAttrs (pkgs.callPackage ./pkgs/wpilib { });
-  frc-nix-update = pkgs.callPackage ./pkgs/frc-nix-update { };
+/**
+  https://github.com/NixOS/nixpkgs/blob/0726f235730331846135184e71d1d1bc3a4b49ad/lib/filesystem.nix#L235-L361
 
-  vscode-extensions = prev.vscode-extensions // {
-    wpilibsuite = { inherit (wpilib) vscode-wpilib; };
-  };
+  Transform a directory tree containing package files suitable for
+  `callPackage` into a matching nested attribute set of derivations.
+
+  For a directory tree like this:
+
+  my-packages
+  ├── a.nix
+  ├── b
+  │  ├── my-extra-feature.patch
+  │  ├── package.nix
+  │  └── support-definitions.nix
+  └── my-namespace
+   ├── c.nix
+   └── d
+      └── package.nix
+
+  `packagesFromDirectoryRecursive` will produce an attribute set like this:
+
+  {
+    a = pkgs.callPackage ./my-packages/a.nix { };
+    b = pkgs.callPackage ./my-packages/b/package.nix { };
+    my-namespace = {
+      c = pkgs.callPackage ./my-packages/my-namespace/c.nix { };
+      d = pkgs.callPackage ./my-packages/my-namespace/d/package.nix { };
+    };
+  }
+*/
+pkgs.lib.packagesFromDirectoryRecursive {
+  inherit (pkgs) callPackage newScope;
+  directory = ./pkgs;
 }
