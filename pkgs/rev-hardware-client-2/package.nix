9f5d3445e5148ee4494bb7a7ebd0f8f873afd59e
NullCube <nullcub3@gmail.com>
2026-03-20 04:29:30 -0700

    it-works-mostly

--- pkgs/rev-hardware-client-2/package.nix
+++ pkgs/rev-hardware-client-2/package.nix
@@ -1,111 +1,133 @@
 {
   lib,
+  buildFHSEnv,
   stdenv,
-  autoPatchelfHook,
   fetchurl,
 
-  wrapGAppsHook4,
-
+  alsa-lib,
   at-spi2-atk,
+  at-spi2-core,
+  atk,
+  autoPatchelfHook,
   cairo,
+  cups,
+  dbus,
+  expat,
+  fontconfig,
   glib,
   gtk3,
-  pango,
-  libx11,
-
-  cups,
-  libxrandr,
-  libxdamage,
-  libxcursor,
-  libxtst,
-  libgcc,
+  libGL,
   libdrm,
   libgbm,
-  nss,
-  libxxf86vm,
-
-  nspr,
-  atk,
-  expat,
-  libxkbcommon,
-  alsa-lib,
-  dbus,
-  at-spi2-core,
-  libGL,
-  udev,
-  systemdLibs,
-  libxfixes,
-  libxext,
-  libxcomposite,
-  libxshmfence,
+  libgcc,
+  libx11,
   libxcb,
-
-  fontconfig,
+  libxcomposite,
+  libxcursor,
+  libxdamage,
+  libxext,
+  libxfixes,
+  libxkbcommon,
+  libxrandr,
+  libxshmfence,
+  libxtst,
+  libxxf86vm,
+  nspr,
+  nss,
+  pango,
+  systemdLibs,
+  udev,
+  wrapGAppsHook3,
 }:
-stdenv.mkDerivation (finalAttrs: {
+let
   pname = "rev-hardware-client-2";
   version = "1.0.7";
 
-  src = fetchurl {
-    url = "https://rhc2.revrobotics.com/download/rev-hardware-client-1.0.7-linux-amd64.tar.gz";
-    hash = "sha256-dIn32bBVarRVoHSG0cZiIt5W33Q5YuQToaZvN/Q8p7s=";
-  };
+  rhc2-unwrapped = stdenv.mkDerivation (finalAttrs: {
+    pname = "${pname}-unwrapped";
+    inherit version;
+    src = fetchurl {
+      url = "https://rhc2.revrobotics.com/download/rev-hardware-client-1.0.7-linux-amd64.tar.gz";
+      hash = "sha256-dIn32bBVarRVoHSG0cZiIt5W33Q5YuQToaZvN/Q8p7s=";
+    };
 
-  nativeBuildInputs = [
-    # wrapGAppsHook3
-    wrapGAppsHook4
-    autoPatchelfHook
-  ];
+    nativeBuildInputs = [
+      autoPatchelfHook
+      wrapGAppsHook3
+    ];
 
-  buildInputs = [
-    alsa-lib
-    at-spi2-atk
-    at-spi2-core
-    atk
-    cairo
-    cairo
-    cups
-    cups.lib
-    dbus
-    expat
-    glib
-    gtk3
-    libGL
-    libdrm
-    libgbm
-    libgcc
-    libx11
-    libxcb
-    libxcomposite
-    libxcursor
-    libxdamage
-    libxext
-    libxfixes
-    libxkbcommon
-    libxrandr
-    libxshmfence
-    libxtst
-    libxxf86vm
-    nspr
-    nss
-    pango
-    stdenv.cc.cc
-    systemdLibs
-    udev
+    buildInputs = [
+      alsa-lib
+      at-spi2-atk
+      at-spi2-core
+      atk
+      cairo
+      cairo
+      cups
+      cups.lib
+      dbus
+      expat
+      glib
+      gtk3
+      libdrm
+      libgbm
+      libgcc
+      libx11
+      libxcb
+      libxcomposite
+      libxcursor
+      libxdamage
+      libxext
+      libxfixes
+      libxkbcommon
+      libxrandr
+      libxshmfence
+      libxtst
+      libxxf86vm
+      nspr
+      nss
+      pango
+      systemdLibs
+      udev
+    ];
+
+    runtimeDependencies = [
+      fontconfig
+      libGL
+      udev
+      stdenv.cc.cc
+    ];
+
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      cp -r . $out/
+      substituteInPlace $out/share/applications/com.revrobotics.revui.rev-hardware-client.desktop \
+      --replace-fail Exec=/usr/lib/rev-robotics/rev-hardware-client/bin/rev-hardware-client Exec=rev-hardware-client
+
+      runHook postInstall
+    '';
+  });
+in
+buildFHSEnv {
+  inherit pname version;
+
+  targetPkgs = _: [
+    rhc2-unwrapped
+
     fontconfig
+    libGL
+    udev
   ];
 
-  dontBuild = true;
+  runScript = "rev-hardware-client";
 
-  installPhase = ''
-    runHook preInstall
-
-    cp -r . $out/
-
-    runHook postInstall
+  extraInstallCommands = ''
+    mkdir -p $out/
+    cp -r ${rhc2-unwrapped}/share $out/share
   '';
-
-  # preFixup = "";
 
   meta = {
     description = "Hardware client for REV devices.";
@@ -115,4 +137,4 @@ stdenv.mkDerivation (finalAttrs: {
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     platforms = [ "x86_64-linux" ];
   };
-})
+}
