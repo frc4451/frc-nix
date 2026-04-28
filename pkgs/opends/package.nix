a328935843f1172325238e729db7d74286542ceb
Kieran Klukas <kieran@dunkirk.sh>
2026-04-28 16:59:45 -0400

    opends: fix hash and add auto updates

--- pkgs/opends/package.nix
+++ pkgs/opends/package.nix
@@ -16,8 +16,9 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/Boomaa23/open-ds/releases/download/v${version}/open-ds-v${version}.jar";
-    sha256 = "01d6c0df4e166188cc216326cc453ffcc04c4709ffef55a19cb934c3ab3d9009";
+    hash = "sha256-AdbA304WYYjMIWMmzEU//MBMRwn/71WhnLk0w6s9kAk=";
   };
+
   dontUnpack = true;
   nativeBuildInputs = [makeWrapper autoPatchelfHook jre];
   buildInputs = [libxkbcommon libx11 libxtst libxt];
@@ -28,12 +29,10 @@ stdenv.mkDerivation rec {
 
     jar xf $out/share/java/${name}-${version}.jar
 
-
     makeWrapper ${jre}/bin/java $out/bin/OpenDS \
       --add-flags "-Djnativehook.lib.path=/tmp" \
       --add-flags "-jar $out/share/java/${name}-${version}.jar" \
       --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [libxkbcommon libx11 libxtst libxt]}
-
   '';
 
   meta = {
