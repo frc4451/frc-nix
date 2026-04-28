79bde045688c944ed9b95cb826bad8224364ea74
zentag <38821230+zentag@users.noreply.github.com>
2026-04-28 11:28:27 -0400

    make opends work by adding necessary libs

--- pkgs/opends/package.nix
+++ pkgs/opends/package.nix
@@ -1,34 +1,39 @@
 {
   stdenv,
-  fetchFromGitHub,
+  autoPatchelfHook,
+  fetchurl,
   jre,
   makeWrapper,
   lib,
+  libxkbcommon,
+  xorg,
 }:
 stdenv.mkDerivation rec {
   name = "OpenDS";
   version = "0.3.1";
 
-  src = fetchFromGitHub {
-    owner = "Boomaa23";
-    repo = "open-ds";
-    rev = "v${version}";
-    hash = "sha256-01d6c0df4e166188cc216326cc453ffcc04c4709ffef55a19cb934c3ab3d9009";
+  src = fetchurl {
+    url = "https://github.com/Boomaa23/open-ds/releases/download/v${version}/open-ds-v${version}.jar";
+    sha256 = "01d6c0df4e166188cc216326cc453ffcc04c4709ffef55a19cb934c3ab3d9009";
   };
   dontUnpack = true;
-  nativeBuildInputs = [makeWrapper];
-
+  nativeBuildInputs = [makeWrapper autoPatchelfHook jre];
+  buildInputs = [libxkbcommon xorg.libX11 xorg.libXtst xorg.libXt];
   installPhase = ''
     mkdir -pv $out/share/java $out/bin
+
     cp ${src} $out/share/java/${name}-${version}.jar
 
+    jar xf $out/share/java/${name}-${version}.jar
+
+
     makeWrapper ${jre}/bin/java $out/bin/OpenDS \
+      --add-flags "-Djnativehook.lib.path=/tmp" \
       --add-flags "-jar $out/share/java/${name}-${version}.jar" \
-      --set _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on' \
-      --set _JAVA_AWT_WM_NONREPARENTING 1
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [libxkbcommon xorg.libX11 xorg.libXtst xorg.libXt]}
+
   '';
 
-  # Some easy metadata, in case I forget.
   meta = {
     homepage = "https://boomaa23.github.io/open-ds";
     description = "An open source version of FRC's DriverStation";
